61257c16a701218ee48b08e3c016b471a0416e9c
Austin Mroz <AustinMroz@utexas.edu>
2023-12-06 14:35:55 -0600

    Texture overrides, mask support for SpliceDenoised

--- nodes.py
+++ nodes.py
@@ -1,6 +1,7 @@
 import torch
 
 import torch.nn.functional as F
+from comfy.sample import prepare_mask
 from comfy_extras.nodes_post_processing import gaussian_kernel
 
 class LogSigmas:
@@ -38,7 +39,8 @@ class SpliceLatents:
                              "size": ("INT", {"default": 4, "min": 1, "step": 1}),
                              "wetness": ("FLOAT", {"default": 1.0, "max": 1,
                                                    "min": 0, "precision": 3,
-                                                   "step": 0.1, "round": .01})},
+                                                   "step": 0.1, "round": .01}),
+                             "texture_override": (["None", "Upper", "Lower"],)},
                 "optional": {"lower": ("LATENT",),
                              "upper": ("LATENT",)}}
 
@@ -46,7 +48,8 @@ class SpliceLatents:
     RETURN_TYPES = ("LATENT",)
     CATEGORY = "latent/advanced"
 
-    def splice_latents(s, mult, size, wetness=1.0, lower=None, upper=None):
+    def splice_latents(s, mult, size, texture_override, wetness=1.0, lower=None, upper=None):
+        #TODO: Find solution to prevent errors when nodes are muted in workflow
         if lower is None and upper is None:
             raise "lower and upper can't both be none"
         if lower is None:
@@ -58,15 +61,27 @@ class SpliceLatents:
         else:
             upper = upper['samples']
         radius = size
-        kernel = gaussian_kernel(radius * 2 + 1, mult, device=lower.device).repeat(4,1,1).unsqueeze(1)
+        length = radius * 2 + 1
+        #for 1.5 channel 3 is texture, Its feasible to
+        #create a kernel that blurs channels [0,1,3] and zeros 2
+        #this conceptually delegates sub-pixel texture to upper always,
+        #but is less viable for mixed configuration
+        #further experimentation is needed
+        mask = gaussian_kernel(length, mult, device=lower.device)
+        kernel = torch.stack((mask, mask, mask, mask)).unsqueeze(1)
 
         lower_b = gaussian_blur(lower, kernel, radius)
         upper_b = gaussian_blur(upper, kernel, radius)
         upper_e = upper - upper_b
         lower_out = lower_b * wetness + lower * (1 - wetness)
         upper_out = upper_e * wetness + upper * (1 - wetness)
+        out = lower_out + upper_out
+        if texture_override == "Upper":
+            out[:,2] = upper[:,2]
+        elif texture_override == "Lower":
+            out[:,2] = lower[:,2]
 
-        return ({"samples": lower_out + upper_out},)
+        return ({"samples": out},)
 
 class SpliceDenoised:
     """A convenience node to splice latents when both noised and denoised outputs exist"""
@@ -83,8 +98,16 @@ class SpliceDenoised:
     CATEGORY = "_for_testing"
 
     def splice_denoised(self, noised_latent, denoised_latent, donor_latent):
-        samples = noised_latent['samples'] - denoised_latent['samples'] + donor_latent['samples']
-        return ({"samples": samples},)
+        #Partial mask support for donor latent
+        donor_samples = donor_latent['samples']
+        if 'noise_mask' in donor_latent:
+            mask = prepare_mask(donor_latent['noise_mask'], donor_samples.shape, 'cpu')
+            donor_samples = donor_samples * (1 - mask)
+            donor_samples = donor_samples + denoised_latent['samples'] * mask
+        samples = noised_latent['samples'] - denoised_latent['samples'] + donor_samples
+        out = noised_latent.copy()
+        out['samples'] = samples
+        return (out,)
 
 NODE_CLASS_MAPPINGS = {
     "LogSigmas": LogSigmas,
